fb61886c65a8676c2cc6d2f90fad8c09c9e7261c
Heshan <mheshan27@gmail.com>
2022-12-09 19:05:53 +0530

    gitignore update

--- server/dist/api/v1/app.js
+++ server/dist/api/v1/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const logger_1 = __importDefault(require("./utility/logger"));
@@ -12,17 +14,17 @@ const app = (0, express_1.default)();
 dotenv_1.default.config();
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 app.use("/api/v1/auth/", authRoute_1.default);
 (0, dbConfig_1.default)(process.env.MONGO_URI)
-    .then(() => {
+  .then(() => {
     logger_1.default.info("Connected to MongoDB");
     app.listen(5000, () => {
-        logger_1.default.info("Server running on port 5000");
+      logger_1.default.info("Server running on port 5000");
     });
-})
-    .catch((error) => {
+  })
+  .catch((error) => {
     logger_1.default.error(error.message);
-});
-//# sourceMappingURL=app.js.map
+  });
+//# sourceMappingURL=app.js.mapss
